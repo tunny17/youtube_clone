32ad654b85bd44d1f7ca229ac2ebcba4bb498eb2
tunny17 <tvnji17@gmail.com>
2022-12-21 21:12:04 +0100

    - fetched the API and created a template off of it

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 import { Sidebar, Videos } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 
 const Feed = () => {
